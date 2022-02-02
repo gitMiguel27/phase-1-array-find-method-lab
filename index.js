e6557c7657a9299ee9460a6c4e13ade4d57da938
Miguel Nazario <mnazario35@gmail.com>
2022-02-02 14:15:58 -0500

    commit tree c89129028d34c634277047df4481627d8ac9623f

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(array) {
+    const findWin = array.find(function(element) {
+        if (element.result === 'W') {
+            return element.year;
+        }
+    })
+    if (findWin !== undefined) {
+        return findWin.year;
+    }
+}
